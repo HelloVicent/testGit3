e0e8a9c57b610ad87123aa3bcc34310397fc0a84
Vicent <huanggang1109@163.com>
2015-08-28 16:26:49 +0800

    Initial Commit

---
+++ testGit3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit3
+//
+//  Created by Vicent on 15/8/28.
+//  Copyright (c) 2015年 Vicent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit3
+//
+//  Created by Vicent on 15/8/28.
+//  Copyright (c) 2015年 Vicent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
